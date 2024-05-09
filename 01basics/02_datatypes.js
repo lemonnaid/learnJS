4b796c07567027aa87addd62061f46f9d7882cc1
lemonnaid <bindalearning@gmail.com>
2024-05-09 16:51:47 +0545

    js datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser so it will not work . If we write this code on browser it will give the resule 6 as a popup 
+
+let name = "Binda" // string datatype
+let age=  100 // integer, number
+let isHappy  = true // boolean
+let country;
+
+// number => 2 to power 53
+// big int is used to large numbers
+// string => "" 
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "binda");  // typeof will give the type.
+console.log(typeof null); // null is object
